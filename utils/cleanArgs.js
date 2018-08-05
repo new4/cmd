ad2b1c4ea70ea28f99286691a11130f6030ba960
new4 <new4wang@163.com>
2018-08-05 23:56:21 +0800

    add xbin

---
+++ utils/cleanArgs.js
@@ -0,0 +1,14 @@
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+module.exports = function cleanArgs(cmd) {
+  const args = {};
+  cmd.options.forEach((o) => {
+    const key = o.long.replace(/^--/, '');
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key];
+    }
+  });
+  return args;
+};
