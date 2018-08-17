dc708a983abd0ff3316e01181c7076dfe8224261
new4 <new4wang@163.com>
2018-08-17 19:24:17 +0800

    self xmocktrans

--- bin/xbin/utils.js
+++ bin/xbin/utils.js
@@ -26,9 +26,21 @@ exports.template = `#!/usr/bin/env node
 const program = require('commander');
 const { tipEnhance } = require('../../utils');
 
+const example = require('./example');
+
 program
   .version('0.1.0')
   .usage('<command> [options]');
+
+/**
+ * example
+ */
+program
+  .command('example <cmd-name>')
+  .alias('ex')
+  .description('a example commander')
+  .option('-p, --params', 'option')
+  .action((name, cmd) => example(name, cmd));
 
 // 强化的提示
 tipEnhance(program, __filename);
