7473e6d317baecdbecae524576481d771df62a11
new4 <new4wang@163.com>
2018-08-23 00:30:17 +0800

    complete xmockdata

--- bin/xmockdata/create.js
+++ bin/xmockdata/create.js
@@ -1,9 +1,11 @@
+const fse = require('fs-extra');
 const chalk = require('chalk');
 
 const {
   log: {
     log,
   },
+  underPath,
 } = require('../../utils');
 
 const MAP_LIST = [
@@ -18,21 +20,13 @@ const MAP_LIST = [
   2399006,
 ];
 
-const SUNDAY = 0;
-const SATURDAY = 6;
-
-const TODAY = '2018-08-22';
-
-const MONTH_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const DAY_LIST = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
 function generateDateStr(monthLen = 12, mockStr) {
 
   const {
     curYear,
     curMonth,
     curDay,
-    curDate,
+    // curDate,
   } = getCurDate(mockStr);
 
   let startYear;
@@ -61,18 +55,24 @@ function generateDateStr(monthLen = 12, mockStr) {
 
   startMonth = addonZero(startMonth);
 
-  const str1 = `${chalk.red(addonZero(delta1))} year ${chalk.red(addonZero(delta2))} month before`;
-  const str2 = `year: ${chalk.yellow(curYear)} => ${chalk.cyan(startYear)}`;
-  const str3 = `month: ${chalk.yellow(curMonth)} => ${chalk.cyan(startMonth)}`;
-  log(` ${str1} | ${str2} | ${str3} `);
+  // const str1 = `${chalk.red(addonZero(delta1))} year ${chalk.red(addonZero(delta2))} month before`;
+  // const str2 = `year: ${chalk.yellow(curYear)} => ${chalk.cyan(startYear)}`;
+  // const str3 = `month: ${chalk.yellow(curMonth)} => ${chalk.cyan(startMonth)}`;
+  // log(` ${str1} | ${str2} | ${str3} `);
 
+  let result = [];
+
+  for (let i = 0, len = curYear - startYear; i <= len; i++) {
+    const startM = i === 0 ? startMonth : '01';
+    const endM = i === len ? curMonth : '12';
+    // log(`fillDateStrOneYear(${+startYear + i}, ${startM}, ${endM}, ${startDay}, ${i === 0}, ${i === len})`);
+    result = result.concat(fillDateStrOneYear(+startYear + i, startM, endM, startDay, i === 0, i === len));
+  }
+  return result;
 }
 
-function fillDateStrOneYear(year, startMonth, endMonth, day) {
-  if (startMonth === endMonth) {
-    return [];
-  }
-  const result = [];
+function fillDateStrOneYear(year, startMonth, endMonth, day, isFirstYear, isLastYear) {
+  let result = [];
   const dayInMonth = getDayInMonth(year);
   const startMonthIndex = +startMonth - 1;
   const endMonthIndex = +endMonth - 1;
@@ -81,20 +81,19 @@ function fillDateStrOneYear(year, startMonth, endMonth, day) {
   for (let i = 0; i <= delta; i++) {
     const curMonth = +startMonth + i;
     const curMonthIndex = curMonth - 1;
-    const startDay = i === 0 ? day : 1; // 开始的日期
-    const endDay = i === delta ? day : dayInMonth[curMonthIndex]; // 结束的日期
-    result.concat(fillDateStrOneMonth(year, curMonth, startDay, endDay));
+    const startDay = i === 0 && isFirstYear ? day : '01'; // 开始的日期
+    const endDay = i === delta && isLastYear ? day : dayInMonth[curMonthIndex]; // 结束的日期
+    // log(`fillDateStrOneMonth(${year}, ${addonZero(curMonth)}, ${startDay}, ${endDay})`);
+    result = result.concat(fillDateStrOneMonth(year, addonZero(curMonth), startDay, endDay));
   }
+  return result;
 }
 
 function fillDateStrOneMonth(year, month, startDay, endDay) {
-  if (startDay === endDay) {
-    return [];
-  }
   const result = [];
-  for (let i = startDay; i < endDay; i++) {
+  for (let i = +startDay; i <= +endDay; i++) {
     result.push(`${year}-${month}-${addonZero(i)}`);
-    log(`${year}-${month}-${addonZero(i)}`);
+    // log(`${year}-${month}-${addonZero(i)}`);
   }
   return result;
 }
@@ -111,7 +110,6 @@ function getDayInMonth(year) {
 }
 
 function getCurDate(mockStr) {
-
   if (mockStr) {
     const arr = mockStr.split('-');
     return {
@@ -137,22 +135,18 @@ function getCurDate(mockStr) {
 }
 
 function addonZero(numStr) {
-  return numStr < 10 ? `0${numStr}` : `${numStr}`;
+  return +numStr < 10 ? `0${numStr}` : `${numStr}`;
 }
 
 module.exports = function create(cmd) {
-  const result = {
-    code: 0,
-    data: {},
-    message: '',
-  };
-  log();
-
-  // log(chalk.cyan(`${generateDateStr()}`));
-
-  for (let i = 0; i < 40; i++) {
-    generateDateStr(i, '2018-03-07');
-  }
-
-  log();
+  fse.outputFile(
+    underPath('cur', '_mock/temp.json'),
+    `${JSON.stringify({
+      code: 0,
+      data: {
+        0: generateDateStr(13, '2018-03-07'),
+      },
+      message: '',
+    }, null, 2)}`,
+  );
 };
