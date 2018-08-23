da15ca1ba4c34d90ee4fb7523e1e1ea4776ea7c1
new4 <new4wang@163.com>
2018-08-23 22:27:57 +0800

    use cn

--- bin/xbin/xbin.js
+++ bin/xbin/xbin.js
@@ -6,6 +6,8 @@ const {
   tipEnhance,
 } = require('../../utils');
 
+const chalk = require('chalk');
+
 const create = require('./create');
 const list = require('./list');
 const remove = require('./remove');
@@ -17,65 +19,45 @@ program
   .version('0.1.0')
   .usage('<command> [options]');
 
-/**
- * 创建一个命令
- */
 program
   .command('create <cmd-name>')
   .alias('c')
-  .description('create a bin commander')
+  .description('创建一个命令')
   // .option('-p, --private', 'create a private cmd') // 不想上传到 git 的，可以带上参数 -p，这样仅限本地进行使用
   .action((name, cmd) => create(name, cmd));
 
-/**
- * 显示当前已有的命令
- */
-program
-  .command('list')
-  .alias('l')
-  .description('show commander list')
-  // .option('-p, --private', 'create a private cmd') // 不想上传到 git 的，可以带上参数 -p，这样仅限本地进行使用
-  .action(() => list());
-
-/**
- * 移除一个命令，需要带上参数 -f 才是真正的移除
- */
 program
   .command('remove <cmd-name>')
   .alias('rm')
-  .description('remove a bin commander')
-  .option('-f, --force', 'remove force')
+  .description('移除一个命令，需要带上参数 -f 才是真正的移除')
+  .option('-f, --force', '强制移除')
   .action((name, cmd) => remove(name, cmd));
 
-/**
- * 重命名一个命令
- */
 program
-  .command('rename <old-name> <new-name>')
+  .command('rename <old-cmd> <new-cmd>')
   .alias('rn')
-  .description('rename a bin commander')
+  .description('重命名一个命令')
   .action((oldName, newName) => rename(oldName, newName));
 
-/**
- * 检查所有的命令，保证 package.json 和命令目录一致
- * 提供参数 -c 可以清理掉这些残留
- */
 program
   .command('check')
   .alias('chk')
-  .description('check commander residue')
-  .option('-c, --clean', 'check and clean commander residue')
+  .description('检查有无命令文件/配置残留')
+  .option('-c, --clean', '清除残留内容')
   .action(cmd => check(cmd));
 
-/**
- * 检查所有的命令，保证 package.json 和命令目录一致
- * 提供参数 -c 可以清理掉这些残留
- */
 program
   .command('relink')
   .alias('rlk')
-  .description('relink all commanders')
+  .description('重新链接所有命令')
   .action(() => relink());
+
+program
+  .command('list')
+  .alias('l')
+  .description('显示当前已有命令列表')
+  // .option('-p, --private', 'create a private cmd') // 不想上传到 git 的，可以带上参数 -p，这样仅限本地进行使用
+  .action(() => list());
 
 // 强化的提示
 tipEnhance(program, __filename);
