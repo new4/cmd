76c364a5a364a638a85c2234b4033a761630fd02
new4 <new4wang@163.com>
2018-09-28 00:45:22 +0800

    utils

--- utils/index.js
+++ utils/index.js
@@ -1,29 +1,13 @@
-const addonZero = require('./addonZero');
-const cleanArgs = require('./cleanArgs');
-const cmd = require('./cmd');
-const icons = require('./icons');
-const log = require('./log');
-const colorStr = require('./colorStr');
-const packageJson = require('./packageJson');
-const reLink = require('./reLink');
-const spinner = require('./spinner');
-const strAlign = require('./strAlign');
-const tipEnhance = require('./tipEnhance');
-const underPath = require('./underPath');
-const yarn = require('./yarn');
-
-module.exports = {
-  addonZero,
-  cleanArgs,
-  cmd,
-  icons,
-  log,
-  colorStr,
-  packageJson,
-  reLink,
-  spinner,
-  strAlign,
-  tipEnhance,
-  underPath,
-  yarn,
-};
+exports.addonZero = require('./addonZero');
+exports.cleanArgs = require('./cleanArgs');
+exports.cmd = require('./cmd');
+exports.icons = require('./icons');
+exports.log = require('./log');
+exports.colorStr = require('./colorStr');
+exports.packageJson = require('./packageJson');
+exports.reLink = require('./reLink');
+exports.spinner = require('./spinner');
+exports.strAlign = require('./strAlign');
+exports.tipEnhance = require('./tipEnhance');
+exports.underPath = require('./underPath');
+exports.yarn = require('./yarn');
