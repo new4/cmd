5bd309914e7dfe398342a639adbead45e4092cab
new4 <new4wang@163.com>
2018-10-28 15:31:48 +0800

    提取color

---
+++ utils/color.js
@@ -0,0 +1,17 @@
+/**
+ * 将一些颜色暴露出去
+ * 色彩取自 bootstrap 的 scss/_variables.scss (https://github.com/twbs/bootstrap)
+ */
+module.exports = {
+  red: '#dc3545',
+  yellow: '#ffc107',
+  blue: '#007bff',
+  green: '#28a745',
+  grey: '#868e96',
+  indigo: '#6610f2',
+  purple: '#6f42c1',
+  pink: '#e83e8c',
+  orange: '#fd7e14',
+  teal: '#20c997',
+  cyan: '#17a2b8',
+};
--- utils/colorStr.js
+++ utils/colorStr.js
@@ -8,26 +8,21 @@ const {
   jsonStringify,
 } = require('./jsonOp');
 
-const colors = {
-  red: '#F04134', // 热情/警示
-  green: '#00A854', // 通过/安全
-  blue: '#108EE9', // 专业/科技
-  pink: '#F5317F', // 典雅/明快/女性
-  orange: '#F56A00', // 醒目/温暖
-  purple: '#7265E6', // 高雅/浪漫
-  yellow: '#FFBF00', // 活力/提示
-  cyan: '#00A2AE', // 清新/冷静/结构化
-  grey: '#919191', // 平稳/中性
+const colors = require('./color');
+
+/**
+ * 生成带 color 颜色的字串 str
+ */
+const createColorStr = (str, color) => {
+  if (isObjectLike(str)) {
+    str = jsonStringify(str);
+  }
+  return chalk.hex(color)(str);
 };
 
 /**
  * 带颜色的字符串
  */
 Object.entries(colors).forEach(([name, color]) => {
-  exports[name] = (str) => {
-    if (isObjectLike(str)) {
-      str = jsonStringify(str);
-    }
-    return chalk.hex(color)(str);
-  };
+  exports[name] = str => createColorStr(str, color);
 });
