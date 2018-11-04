815fbbef37161b3b3462ff305b8c0a8b0eb27c36
new4 <new4wang@163.com>
2018-11-04 15:51:04 +0800

    add unity to xbin

--- bin/xleetcode/utils/index.js
+++ bin/xleetcode/utils/index.js
@@ -1,4 +1,4 @@
+exports.generateHeaders = require('./generateHeaders');
 exports.getAllProblems = require('./getAllProblems');
 exports.getRespSetCookieInfo = require('./getRespSetCookieInfo');
-exports.generateHeaders = require('./generateHeaders');
 exports.requestCsrftoken = require('./requestCsrftoken');
