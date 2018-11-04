ab32c4c4cd00706a90bf98064624834c0fff52f1
new4 <new4wang@163.com>
2018-11-04 20:48:45 +0800

    rewrite shouldBe

--- utils/cleanArgs.js
+++ utils/cleanArgs.js
@@ -5,8 +5,6 @@ module.exports = function cleanArgs(cmd) {
   const args = {};
   cmd.options.forEach((o) => {
     const key = o.long.replace(/^--/, '');
-    // if an option is not present and Command has a method with the same name
-    // it should not be copied
     if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
       args[key] = cmd[key];
     }
