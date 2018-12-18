41932d13b83513cd2a9b514ff5e0cce16ceb880a
new4 <new4wang@163.com>
2018-12-18 23:01:04 +0800

    add xtriple

---
+++ bin/xtriple/xtriple.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const program = require('commander');
+
+const {
+  cleanArgs,
+  tipEnhance,
+} = require('../../../utils');
+
+const example = () => console.log('hello world!');
+
+program
+  .version('0.1.0')
+  .usage('<command> [options]');
+
+/**
+ * example
+ */
+program
+  .command('hello <cmd-name>')
+  .alias('ex')
+  .description('a example commander')
+  .option('-p, --params', 'option')
+  .action((name, cmd) => example(name, cleanArgs(cmd)));
+
+// 强化的提示
+tipEnhance(program, 'xtriple');
+
+program.parse(process.argv);
