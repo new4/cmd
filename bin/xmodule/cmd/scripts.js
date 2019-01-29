e172bad1255fc41759d54dff0d685d923e26bdca
new4 <new4wang@163.com>
2019-01-29 12:31:22 +0800

    add scripts to xmodule

---
+++ bin/xmodule/cmd/scripts.js
@@ -0,0 +1,51 @@
+const fse = require('fs-extra');
+
+const {
+  colorStr: {
+    yellow,
+    cyan,
+  },
+  underPath,
+  icons: {
+    hollowCircle,
+  },
+  log: {
+    log,
+  },
+  shouldBe: {
+    sbValidValue,
+    sbValidDir,
+    sbValidPlainObject,
+  },
+  fileOp: {
+    getExistFiles,
+  },
+  strAdjust: {
+    strAdjustRight,
+  },
+} = require('../../../utils');
+
+/**
+ * 显示当前项目 package.json 中的 scripts 脚本
+ */
+module.exports = (dir) => {
+  const targetDir = underPath('cur', dir);
+
+  sbValidDir(targetDir, `invalid path: ${yellow(dir)}`);
+
+  const [pkgJson] = getExistFiles(targetDir, file => file === 'package.json');
+
+  sbValidValue(pkgJson, `no ${yellow('package.json')} under: ${targetDir}`);
+
+  const { scripts } = fse.readJsonSync(underPath(targetDir, 'package.json'));
+
+  sbValidPlainObject(scripts, `no ${yellow('scripts')} in package.json`);
+
+  log();
+  Object
+    .entries(scripts)
+    .forEach(([key, value]) => {
+      log(` ${cyan(hollowCircle)} ${yellow(strAdjustRight(key, 10))} ${value}`);
+    });
+  log();
+};
--- bin/xmodule/xmodule.js
+++ bin/xmodule/xmodule.js
@@ -8,6 +8,7 @@ const {
 
 const init = require('./cmd/init');
 const unify = require('./cmd/unify');
+const scripts = require('./cmd/scripts');
 
 program
   .version('0.1.0')
@@ -28,6 +29,14 @@ program
   .command('unify <dir>')
   .description('unify a directory as a module')
   .action(dir => unify(dir));
+
+/**
+ * 显示当前项目 package.json 中的 scripts 脚本
+ */
+program
+  .command('scripts <dir>')
+  .description('list scripts in package.json')
+  .action(dir => scripts(dir));
 
 // 强化的提示
 tipEnhance(program, 'xmodule');
