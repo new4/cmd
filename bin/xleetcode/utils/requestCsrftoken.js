bd89de29889e0ace420279bf4fb97fb617309fcd
new4 <new4wang@163.com>
2019-01-29 23:06:35 +0800

    chore(filename): add const {

---
+++ bin/xleetcode/utils/requestCsrftoken.js
@@ -0,0 +1,22 @@
+const {
+  requestP,
+} = require('../../../utils');
+
+const getRespSetCookieInfo = require('./getRespSetCookieInfo');
+
+const {
+  url: {
+    loginUrl,
+  },
+} = require('../config');
+
+/**
+ * 为了获取 csrftoken 而发的请求
+ * 默认使用登录的 url，其实任意一个 url 都能够获取
+ */
+module.exports = async (url = loginUrl) => {
+  const [response] = await requestP({
+    url,
+  });
+  return getRespSetCookieInfo(response, 'csrftoken');
+};
